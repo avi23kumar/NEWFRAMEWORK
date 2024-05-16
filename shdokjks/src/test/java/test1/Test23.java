59144b5c4c84e46348baf2d8d82ec9ab2fd2c379
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-16 17:13:50 +0530

    4

--- shdokjks/src/test/java/test1/Test23.java
+++ shdokjks/src/test/java/test1/Test23.java
@@ -6,14 +6,14 @@ public class Test23 {
 	@Test
 	public void test25test()
 	{
-		String URL = System.getProperty("url");
+	/**	String URL = System.getProperty("url");
 		String BROWSER = System.getProperty("browser");
 		String USERNAME = System.getProperty("username");
 		String PAssword = System.getProperty("password");
 		System.out.println(URL);
 		System.out.println(BROWSER);
 		System.out.println(USERNAME);
-		System.out.println(PAssword);
+		System.out.println(PAssword);**/
 		System.out.println("print one c");
 	}
 
