a360d6e4a3c18119c9f5a977ba13d62a4f6772e5
Avinashkumar s m <avinashkumar1998dgd@gmail.com>
2024-05-16 16:21:06 +0530

    first push

---
+++ shdokjks/src/test/java/test1/Test23.java
@@ -0,0 +1,26 @@
+package test1;
+
+import org.testng.annotations.Test;
+
+public class Test23 {
+	@Test
+	public void test25test()
+	{
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PAssword = System.getProperty("password");
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PAssword);
+		System.out.println("print one c");
+	}
+
+	@Test
+	public void test26test()
+	{
+		System.out.println("print one c");
+	}
+
+}
---
+++ shdokjks/src/test/java/test2/Ktest.java
@@ -0,0 +1,22 @@
+package test2;
+
+import org.testng.annotations.Test;
+
+public class Ktest {
+	public class Test23 {
+		@Test
+		public void test25test()
+		{
+			System.out.println("print one d");
+		}
+
+		@Test
+		public void test26test()
+		{
+			System.out.println("print one h");
+		}
+
+
+}
+}
+
